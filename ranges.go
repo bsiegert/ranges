0bd485935f4f589a500221f17b4c07f5057afbbd
Benny Siegert <bsiegert@gmail.com>
2011-12-16 16:52:32 +0100

    Initial revision.
    Copied over from private project repository.

---
+++ ranges.go
@@ -0,0 +1,38 @@
+package ranges
+
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
+
+func Parse(r string) ([]int, error) {
+	var expanded []int
+
+	for _, item := range strings.Split(r, ",") {
+		lohi := strings.Split(item, "-")
+		switch len(lohi) {
+		case 1:
+			v, err := strconv.Atoi(item)
+			if err != nil {
+				return nil, err
+			}
+			expanded = append(expanded, v)
+		case 2:
+			lo, err := strconv.Atoi(item)
+			if err != nil {
+				return nil, err
+			}
+			hi, err := strconv.Atoi(item)
+			if err != nil {
+				return nil, err
+			}
+			for i := lo; i <= hi; i++ {
+				expanded = append(expanded, i)
+			}
+		default:
+			return nil, fmt.Errorf("invalid range: %s", item)
+		}
+	}
+	return expanded, nil
+}
