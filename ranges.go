c34e2dc73f8d52b4e95b0b97a2b4028491152af0
Benny Siegert <bsiegert@gmail.com>
2011-12-21 11:45:51 +0100

    Add some documentation, Contains and Expand methods for IntRange.

--- ranges.go
+++ ranges.go
@@ -1,3 +1,9 @@
+// Package ranges contains tools for working with integer ranges.
+// 
+// An "integer range" allows to give a set of numbers as a string,
+// which can be parsed by a call to Parse. The result can be obtained
+// as a slice of integers by calling Expand or be tested against with
+// Contains.
 package ranges
 
 import (
@@ -5,6 +11,29 @@ import (
 	"strconv"
 	"strings"
 )
+
+// An IntRange is a single component of an integer range expression.
+type IntRange struct {
+	Lo, Hi int
+}
+
+// IntRanges is a slice of multiple integer ranges, allowing the
+// expression of non-contiguous ranges (for example "1,3-4").
+type IntRanges []IntRange
+
+// Contains returns true if ir contains value.
+func (ir IntRange) Contains(value int) bool {
+	return value >= ir.Lo && value <= ir.Hi
+}
+
+// Expand returns a slice of integers that contains all the numbers in ir.
+func (ir IntRange) Expand() []int {
+	e := make([]int, 0, ir.Hi-ir.Lo+1)
+	for i := ir.Lo; i <= ir.Hi; i++ {
+		e = append(e, i)
+	}
+	return e
+}
 
 func Parse(r string) ([]int, error) {
 	var expanded []int
